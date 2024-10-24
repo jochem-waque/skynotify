ecad1fa2ccab6ac2ea683ed7d548993567802a35
Jochem-W <16637577+Jochem-W@users.noreply.github.com>
2024-10-24 22:31:42 +0200

    Change type annotations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,10 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 })
 
-export default [
+/** @type {import('eslint').Linter.Config[]} */
+const configs = [
   ...compat.extends("next/core-web-vitals"),
   ...compat.extends("next/typescript"),
 ]
+
+export default configs
--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-/** @type {import('next').NextConfig} */
 import withSerwistInit from "@serwist/next"
 
 const withSerwist = withSerwistInit({
