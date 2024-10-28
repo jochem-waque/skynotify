f07bd298dd06493e11808310d27c67e627867cfe
Jochem-W <16637577+Jochem-W@users.noreply.github.com>
2024-10-28 17:37:41 +0100

    Move Firebase config to app root

--- app/util/firebase.ts
+++ app/util/firebase.ts
@@ -3,7 +3,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-import { Config } from "../../firebase"
+import { Config } from "@/firebase"
 import { initializeApp } from "firebase/app"
 
 const FirebaseApp = initializeApp(Config)
