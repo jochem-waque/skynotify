6f73f1c2ebc45127334330ffdcaa0c9eb86b7664
Jochem-W <16637577+Jochem-W@users.noreply.github.com>
2024-10-28 17:39:49 +0100

    Set start_url to root

--- app/app/manifest.ts
+++ app/app/manifest.ts
@@ -12,7 +12,7 @@ export default function manifest(): MetadataRoute.Manifest {
     description: "A Progressive Web App for Bluesky post notifications",
     id: "/",
     scope: "/",
-    start_url: "/configure", // TODO
+    start_url: "/", // TODO
     display: "standalone",
     orientation: "natural",
     launch_handler: {
