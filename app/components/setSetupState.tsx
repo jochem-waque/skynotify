f283c134ce1137173abe9b12adfeae915c042c54
Jochem-W <16637577+Jochem-W@users.noreply.github.com>
2024-10-30 20:51:23 +0100

    Mark as client component

--- app/components/setSetupState.tsx
+++ app/components/setSetupState.tsx
@@ -3,6 +3,8 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+"use client"
+
 import { useDataStore } from "@/util/store"
 import { useEffect } from "react"
 
