304de51980ffb3a8c12b1daedeffbd880ef47ed2
Jochem-W <16637577+Jochem-W@users.noreply.github.com>
2024-11-01 18:19:29 +0100

    Improve privacy policy

--- app/app/privacy/page.tsx
+++ app/app/privacy/page.tsx
@@ -10,16 +10,27 @@ export default function Page() {
   return (
     <>
       <header>
-        <h1 className="text-3xl">Bsky Post Notifications</h1>
+        <h1 className="text-center text-3xl">Bsky Post Notifications</h1>
       </header>
       <main className="flex grow flex-col gap-4">
         <h2 className="text-2xl">Privacy Policy</h2>
         <div className="flex flex-col gap-1">
+          <h3 className="text-xl">Cookies and local data storage</h3>
+          <p>
+            To effectively provide this service, the use of cookies and other
+            forms of local browser storage is required, henceforth collectively
+            referred to as &quot;cookies&quot;. Cookies are used exclusively for
+            the provision of this service, and are not used for any other
+            purposes, such as tracking. Cookies are exclusively placed by this
+            service, and not by any third party.
+          </p>
+        </div>
+        <div className="flex flex-col gap-1">
           <h3 className="text-xl">Data processing</h3>
           <p>
-            When this service is interacted with, data is sent through
-            Cloudflare&apos;s network before arriving at the origin server. As
-            such, the{" "}
+            To help secure this service, users of this service, and the operator
+            of this service, data is sent through Cloudflare&apos;s network
+            before arriving at the origin server. As such, the{" "}
             <Link
               className="text-blue-500 underline transition-opacity hover:opacity-75"
               href="https://www.cloudflare.com/privacypolicy/"
@@ -28,11 +39,12 @@ export default function Page() {
             </Link>{" "}
             applies. For an overview of the data processed by Cloudflare, please
             refer to the &quot;Network&quot; tab in the browser&apos;s Developer
-            tools.
+            tools. This data may also be used to monitor the performance of this
+            service, with the intention of improving performance.
           </p>
           <p>
-            To provide this service, some data is processed by Google to enable
-            the sending and receiving of push notifications. As such, the{" "}
+            To enable the sending and receiving of push notifications, some data
+            is processed by Firebase. As such, the{" "}
             <Link
               className="text-blue-500 underline transition-opacity hover:opacity-75"
               href="https://firebase.google.com/support/privacy"
@@ -48,17 +60,17 @@ export default function Page() {
           </p>
         </div>
         <div className="flex flex-col gap-1">
-          <h3 className="text-xl">Data storage</h3>
+          <h3 className="text-xl">Remote data storage</h3>
           <p>
             To provide this service, the user&apos;s data needs to be stored.
             This data consists of Firebase Cloud Messaging tokens, Bluesky
-            account identifiers, and Boolean values indiciating the notification
+            account identifiers, and Boolean values indicating notification
             preferences. This data is anonymous, and cannot be traced back to
             any individual user.
           </p>
           <p>
-            Additionally, your data might also be stored by third parties. For
-            more information, please refer to the{" "}
+            Additionally, the user&apos;s data might also be stored by third
+            parties. For more information, please refer to the{" "}
             <Link
               className="text-blue-500 underline transition-opacity hover:opacity-75"
               href="https://www.cloudflare.com/privacypolicy/"
@@ -73,6 +85,27 @@ export default function Page() {
               Privacy and Security in Firebase
             </Link>{" "}
             document.
+          </p>
+        </div>
+        <div className="flex flex-col gap-1">
+          <h3 className="text-xl">Personal data</h3>
+          <p>
+            Unless stated in the{" "}
+            <Link
+              className="text-blue-500 underline transition-opacity hover:opacity-75"
+              href="https://www.cloudflare.com/privacypolicy/"
+            >
+              Cloudflare Privacy Policy
+            </Link>{" "}
+            and the{" "}
+            <Link
+              className="text-blue-500 underline transition-opacity hover:opacity-75"
+              href="https://firebase.google.com/support/privacy"
+            >
+              Privacy and Security in Firebase
+            </Link>{" "}
+            document, this service does not make use of any personal data at any
+            point.
           </p>
         </div>
       </main>
