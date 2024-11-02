ee162d28d41cdc7cde7983624b6a3ccb6e624556
Jochem-W <16637577+Jochem-W@users.noreply.github.com>
2024-11-02 20:41:43 +0100

    Improve search bar styling

--- app/components/profile/searchableProfileSelectorList.tsx
+++ app/components/profile/searchableProfileSelectorList.tsx
@@ -6,7 +6,7 @@
 "use client"
 
 import ProfileSelectorList from "./profileSelectorList"
-import { ChangeEvent, useRef, useState } from "react"
+import { ChangeEvent, MouseEvent, useRef, useState } from "react"
 
 export default function SearchableProfileSelectorList() {
   const [query, setQuery] = useState("")
@@ -18,13 +18,31 @@ export default function SearchableProfileSelectorList() {
     timeout.current = setTimeout(() => setQuery(value), 200)
   }
 
+  function click(event: MouseEvent<SVGSVGElement>) {
+    const sibling = event.currentTarget.nextElementSibling
+    if (sibling instanceof HTMLInputElement) {
+      sibling.focus()
+    }
+  }
+
   return (
     <>
-      <input
-        className="rounded-lg bg-neutral-200 p-2 font-mono dark:bg-neutral-800"
-        placeholder="Search following"
-        onChange={change}
-      ></input>
+      <div className="flex rounded-lg bg-neutral-200 dark:bg-neutral-800">
+        <svg
+          onClick={click}
+          className="box-content w-6 cursor-pointer rounded-l-lg fill-current p-2"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="10 10 31.66 31.66"
+        >
+          <path d="M23 36c-7.2 0-13-5.8-13-13s5.8-13 13-13 13 5.8 13 13-5.8 13-13 13zm0-24c-6.1 0-11 4.9-11 11s4.9 11 11 11 11-4.9 11-11-4.9-11-11-11z"></path>
+          <path d="M32.682 31.267l8.98 8.98-1.414 1.414-8.98-8.98z"></path>
+        </svg>
+        <input
+          className="grow rounded-r-lg bg-transparent p-2 font-mono"
+          placeholder="Search following"
+          onChange={change}
+        ></input>
+      </div>
       <div className="flex grow flex-col overflow-y-auto">
         <ProfileSelectorList query={query}></ProfileSelectorList>
       </div>
