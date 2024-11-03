8375b27433a4f01a6e49a18d9d203a68ab6bfcf9
Jochem-W <16637577+Jochem-W@users.noreply.github.com>
2024-11-04 00:31:48 +0100

    Clarify with comment

--- app/config.ts
+++ app/config.ts
@@ -3,4 +3,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+// TODO shouldn't be necessary, but for some reason a NEXT_PUBLIC_ env variable
+// isn't always accessible?
 export const SubscriptionLimit = 50
