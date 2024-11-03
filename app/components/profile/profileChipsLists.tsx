8f8cfbd6188bb4eb787a82abe99f8b440a6b7fe6
Jochem-W <16637577+Jochem-W@users.noreply.github.com>
2024-11-03 16:11:54 +0100

    Add text for empty list

--- app/components/profile/profileChipsLists.tsx
+++ app/components/profile/profileChipsLists.tsx
@@ -14,41 +14,46 @@ export default function ProfileChipsList() {
   const profiles = useDataStore((state) => state.profiles)
   const selected = useDataStore((state) => state.selected)
 
-  return [...profiles.entries()]
-    .filter(([did]) => selected.has(did))
-    .map(([did, profile]) => (
-      <div
-        className="flex flex-col gap-2 rounded-lg bg-neutral-100 p-2 dark:bg-neutral-800"
-        key={did}
-      >
-        <Profile
-          avatar={profile.avatar}
-          handle={profile.handle}
-          displayName={profile.displayName}
-        ></Profile>
-        <form className="flex flex-wrap gap-2">
-          <label className="flex items-center rounded-full bg-neutral-200 px-3 py-1 outline-2 outline-black transition hover:opacity-75 has-[:checked]:bg-blue-400 has-[:focus-visible]:outline dark:bg-neutral-700 dark:outline-white has-[:checked]:dark:bg-blue-600">
-            <span className="select-none">Posts</span>
-            <NotificationChipInput
-              did={did}
-              type={"posts"}
-            ></NotificationChipInput>
-          </label>
-          <label className="flex items-center rounded-full bg-neutral-200 px-3 py-1 outline-2 outline-black transition hover:opacity-75 has-[:checked]:bg-blue-400 has-[:focus-visible]:outline dark:bg-neutral-700 dark:outline-white has-[:checked]:dark:bg-blue-600">
-            <span className="select-none">Reposts</span>
-            <NotificationChipInput
-              did={did}
-              type={"reposts"}
-            ></NotificationChipInput>
-          </label>
-          <label className="flex items-center rounded-full bg-neutral-200 px-3 py-1 outline-2 outline-black transition hover:opacity-75 has-[:checked]:bg-blue-400 has-[:focus-visible]:outline dark:bg-neutral-700 dark:outline-white has-[:checked]:dark:bg-blue-600">
-            <span className="select-none">Replies</span>
-            <NotificationChipInput
-              did={did}
-              type={"replies"}
-            ></NotificationChipInput>
-          </label>
-        </form>
-      </div>
-    ))
+  return (
+    <>
+      {selected.size === 0 && <p>No profiles selected.</p>}
+      {[...profiles.entries()]
+        .filter(([did]) => selected.has(did))
+        .map(([did, profile]) => (
+          <div
+            className="flex flex-col gap-2 rounded-lg bg-neutral-100 p-2 dark:bg-neutral-800"
+            key={did}
+          >
+            <Profile
+              avatar={profile.avatar}
+              handle={profile.handle}
+              displayName={profile.displayName}
+            ></Profile>
+            <form className="flex flex-wrap gap-2">
+              <label className="flex items-center rounded-full bg-neutral-200 px-3 py-1 outline-2 outline-black transition hover:opacity-75 has-[:checked]:bg-blue-400 has-[:focus-visible]:outline dark:bg-neutral-700 dark:outline-white has-[:checked]:dark:bg-blue-600">
+                <span className="select-none">Posts</span>
+                <NotificationChipInput
+                  did={did}
+                  type={"posts"}
+                ></NotificationChipInput>
+              </label>
+              <label className="flex items-center rounded-full bg-neutral-200 px-3 py-1 outline-2 outline-black transition hover:opacity-75 has-[:checked]:bg-blue-400 has-[:focus-visible]:outline dark:bg-neutral-700 dark:outline-white has-[:checked]:dark:bg-blue-600">
+                <span className="select-none">Reposts</span>
+                <NotificationChipInput
+                  did={did}
+                  type={"reposts"}
+                ></NotificationChipInput>
+              </label>
+              <label className="flex items-center rounded-full bg-neutral-200 px-3 py-1 outline-2 outline-black transition hover:opacity-75 has-[:checked]:bg-blue-400 has-[:focus-visible]:outline dark:bg-neutral-700 dark:outline-white has-[:checked]:dark:bg-blue-600">
+                <span className="select-none">Replies</span>
+                <NotificationChipInput
+                  did={did}
+                  type={"replies"}
+                ></NotificationChipInput>
+              </label>
+            </form>
+          </div>
+        ))}
+    </>
+  )
 }
