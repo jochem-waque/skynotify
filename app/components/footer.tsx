f043aad738557954e0c620caf7aebbde3994b282
Jochem-W <16637577+Jochem-W@users.noreply.github.com>
2024-11-03 16:48:29 +0100

    Made with love

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -8,16 +8,22 @@ import Link from "next/link"
 export default function Footer({ absolute }: { absolute?: boolean }) {
   return (
     <footer
-      className={`${absolute ? "absolute bottom-0" : ""} flex w-full justify-center gap-2 text-xs opacity-50`}
+      className={`${absolute ? "absolute bottom-0" : ""} flex w-full flex-wrap justify-center gap-2 text-xs opacity-50`}
     >
-      <p>&copy; {process.env.NEXT_PUBLIC_AUTHOR} 2024</p>
+      <span>
+        Made with <span className="brightness-0 grayscale dark:invert">❤️</span>
+      </span>
       <span className="border-r border-current"></span>
+      <span>&copy; {process.env.NEXT_PUBLIC_AUTHOR} 2024</span>
+      <span className="border-r border-current"></span>
+
       <Link
         className="underline transition-opacity hover:opacity-75"
         href="/privacy"
       >
         Privacy Policy
       </Link>
+
       <span className="border-r border-current"></span>
       <Link
         className="underline transition-opacity hover:opacity-75"
