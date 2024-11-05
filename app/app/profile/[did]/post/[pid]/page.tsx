c89ac1b5753a37e5889755a82cb7aedf5ab38de1
Jochem-W <16637577+Jochem-W@users.noreply.github.com>
2024-11-05 01:40:50 +0100

    Set link URL to Bluesky

--- app/app/profile/[did]/post/[pid]/page.tsx
+++ app/app/profile/[did]/post/[pid]/page.tsx
@@ -20,9 +20,10 @@ export default async function Page({
   params: Promise<{ pid: string; did: string }>
 }) {
   const { pid, did } = await params
-  let url = new URL(
+  const linkUrl = new URL(
     `https://bsky.app/profile/${decodeURIComponent(did)}/post/${decodeURIComponent(pid)}`,
   )
+  let redirectUrl = new URL(linkUrl)
 
   const hdrs = await headers()
   const userAgent = hdrs.get("User-Agent")
@@ -30,30 +31,28 @@ export default async function Page({
     const parser = new UAParser(userAgent)
     switch (parser.getOS().name) {
       case "Android":
-        url = new URL(
-          `intent:/${url.pathname}#Intent;scheme=bluesky;package=xyz.blueskyweb.app;end`,
+        redirectUrl = new URL(
+          `intent:/${redirectUrl.pathname}#Intent;scheme=bluesky;package=xyz.blueskyweb.app;end`,
         )
         break
       case "iOS":
       case "macOS":
         // TODO should be ipadOS only
-        url = new URL(`bluesky:/${url.pathname}`)
+        redirectUrl = new URL(`bluesky:/${redirectUrl.pathname}`)
         break
       default:
         break
     }
   }
 
-  const href = url.toString()
-
   return (
     <>
-      <Redirect href={href}></Redirect>
+      <Redirect href={redirectUrl.toString()}></Redirect>
       <div className="flex grow flex-col items-center justify-center gap-1">
         <h1 className="text-center text-3xl">Redirecting to Bluesky</h1>
         <Link
           className="text-lg text-blue-500 underline transition-opacity hover:opacity-75"
-          href={href}
+          href={linkUrl.toString()}
         >
           Click here if you&apos;re not redirected
         </Link>
