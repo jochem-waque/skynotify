b317985b5afdf90b14fdc4ce5c739d0124a50754
Jochem-W <16637577+Jochem-W@users.noreply.github.com>
2024-11-07 23:44:18 +0100

    Add robots.txt and sitemap.xml

---
+++ app/app/robots.ts
@@ -0,0 +1,17 @@
+/**
+ * Copyright (C) 2024  Jochem-W
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+import { MetadataRoute } from "next"
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/$",
+      disallow: "/",
+    },
+    sitemap: `https://${process.env.NEXT_PUBLIC_HOSTNAME}/sitemap.xml`,
+  }
+}
---
+++ app/app/sitemap.ts
@@ -0,0 +1,17 @@
+/**
+ * Copyright (C) 2024  Jochem-W
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+import { MetadataRoute } from "next"
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: `https://${process.env.NEXT_PUBLIC_HOSTNAME}/`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+  ]
+}
