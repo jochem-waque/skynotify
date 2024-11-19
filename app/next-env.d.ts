f4c930114f92c3b65c427fdadc1dfbaabc07874c
Jochem-W <16637577+Jochem-W@users.noreply.github.com>
2024-11-19 23:24:51 +0100

    Remove license header from generated file

--- app/next-env.d.ts
+++ app/next-env.d.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (C) 2024  Jochem-W
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
