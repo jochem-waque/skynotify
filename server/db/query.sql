260e693f70c297189e1371d177eb0b6381c8b70b
Jochem-W <16637577+Jochem-W@users.noreply.github.com>
2024-11-19 11:13:32 +0100

    Manually write database functions

--- server/db/query.sql
+++
@@ -1,5 +0,0 @@
--- name: GetSubscriptions :many
-SELECT token.token, posts, reposts, replies FROM subscription INNER JOIN token ON token.id = subscription.token WHERE target = pggen.arg('did') AND unregistered IS NULL;
-
--- name: InvalidateToken :exec
-UPDATE token SET unregistered = NOW() WHERE token = pggen.arg('token');
