a56a487a3364a8fde374bc9a431d5d063d178f61
Jochem-W <16637577+Jochem-W@users.noreply.github.com>
2024-11-26 16:09:58 +0100

    Add more metadata

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -8,6 +8,7 @@ import OpenBackgroundNotifications from "@/components/openBackgroundNotification
 import UpdateToken from "@/components/updateToken"
 import { Metadata, Viewport } from "next"
 import { Noto_Sans, Noto_Sans_Mono } from "next/font/google"
+import Head from "next/head"
 import "./globals.css"
 
 const font = Noto_Sans({
@@ -23,21 +24,26 @@ const mono = Noto_Sans_Mono({
 })
 
 export const metadata: Metadata = {
-  keywords: ["#nobridge"],
+  keywords: ["#nobridge", "bluesky", "notifications", "push", "posts"],
   description:
     "Web app that provides real-time background push notifications for Bluesky posts",
+  applicationName: "SkyNotify",
+  robots: "nofollow",
   openGraph: {
+    type: "website",
+    siteName: "SkyNotify",
     title: "SkyNotify",
     description:
       "Web app that provides real-time background push notifications for Bluesky posts",
-    // images: "/icon-192x192.png",
+    images: "/icon-192x192.png",
+    url: `https://${process.env.NEXT_PUBLIC_HOSTNAME}`,
   },
   twitter: {
     card: "summary",
     title: "SkyNotify",
     description:
       "Web app that provides real-time background push notifications for Bluesky posts",
-    // images: "/icon-192x192.png",
+    images: "/icon-192x192.png",
   },
   metadataBase: new URL(`https://${process.env.NEXT_PUBLIC_HOSTNAME}/`),
 }
@@ -51,6 +57,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${font.variable} ${mono.variable} font-sans`}>
+      <Head>
+        <script key="website-data" type="application/ld+json">
+          {JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "WebSite",
+            name: "SkyNotify",
+            url: "https://${process.env.NEXT_PUBLIC_HOSTNAME}/",
+          })}
+        </script>
+      </Head>
       <body className="mx-auto flex min-h-[100svh] flex-col items-center p-4">
         <div className="container relative flex max-w-xl grow flex-col gap-4">
           {children}
