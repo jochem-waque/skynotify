e6f5d06394dcfca9753d0a2d20884764a5208d22
Jochem-W <16637577+Jochem-W@users.noreply.github.com>
2024-12-20 23:20:43 +0100

    Move all configuration to env

--- app/util/firebase.ts
+++ app/util/firebase.ts
@@ -3,9 +3,16 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-import { Config } from "@/firebase"
 import { initializeApp } from "firebase/app"
 
-const FirebaseApp = initializeApp(Config)
+const FirebaseApp = initializeApp({
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+})
 
 export default FirebaseApp
