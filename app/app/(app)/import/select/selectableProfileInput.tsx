0b5e7a38ef7cd62ffcf01f352e5be4ec7f97f201
Jochem-W <16637577+Jochem-W@users.noreply.github.com>
2024-12-20 16:56:05 +0100

    Don't rerender rest of list when selecting

--- app/app/(app)/import/select/selectableProfileInput.tsx
+++ app/app/(app)/import/select/selectableProfileInput.tsx
@@ -5,20 +5,20 @@
  */
 "use client"
 
-import { SubscriptionLimit } from "@/config"
 import { useDataStore } from "@/util/store"
 import { ProfileView } from "@atproto/api/dist/client/types/app/bsky/actor/defs"
-import { ChangeEvent } from "react"
+import { ChangeEvent, useState } from "react"
 
 export default function SelectableProfileInput({
   did,
 }: Pick<ProfileView, "did">) {
-  const selected = useDataStore((state) => state.selected)
   const setSelected = useDataStore((state) => state.setSelected)
   const setNotifyPosts = useDataStore((state) => state.setNotifyPosts)
-  const isSelected = selected.has(did)
+  const isSelected = useDataStore((state) => state.isSelected)
+  const [thisSelected, setThisSelected] = useState(isSelected(did))
 
   function change(event: ChangeEvent<HTMLInputElement>) {
+    setThisSelected(event.currentTarget.checked)
     setSelected(event.currentTarget.name, event.currentTarget.checked)
     if (event.currentTarget.checked) {
       setNotifyPosts(event.currentTarget.name, true)
@@ -27,10 +27,9 @@ export default function SelectableProfileInput({
 
   return (
     <input
-      disabled={selected.size >= SubscriptionLimit && !isSelected}
       className="h-0 w-0"
       onChange={change}
-      checked={isSelected}
+      checked={thisSelected}
       name={did}
       type="checkbox"
     ></input>
