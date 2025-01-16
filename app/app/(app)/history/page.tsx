f7cb51bac41aecf177904e9c5f1f3493a41e1b1c
Jochem-W <16637577+Jochem-W@users.noreply.github.com>
2025-01-16 13:34:07 +0100

    Add missing license headers

--- app/app/(app)/history/page.tsx
+++ app/app/(app)/history/page.tsx
@@ -1,3 +1,8 @@
+/**
+ * Copyright (C) 2024  Jochem-W
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 import Notifications from "./notifications"
 
 export default function Page() {
